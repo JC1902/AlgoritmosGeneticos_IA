1e86001008664a179dd2bd7039068ed09dbe33df
Jorge Cisneros <jorgecisneros2002@gmail.com>
2024-04-11 13:43:47 -0600

    Avance al excel

--- excel.py
+++ excel.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 cromosoma_1 = [ 12, 5, 23, 8 ]
 cromosoma_2 = [ 2, 21, 18, 3 ]
 cromosoma_3 = [ 10, 4, 13, 14 ]
@@ -17,43 +20,200 @@ def fitness( fx ):
 
 def main():
     generacion = 0
+    global cromosoma_1, cromosoma_2, cromosoma_3, cromosoma_4, cromosoma_5, cromosoma_6
+    ratio_mutacion = 0.1
+    total_genes = 4*6
+
+    mutaciones_totales = math.floor( ratio_mutacion * total_genes )
 
     while ( generacion <= 50 ):    
         totalFitnesses, acum_suma = 0, 0 
-        probabilities, acumulaciones = [], []
+        probabilities, acumulaciones, nuevas_pos, nuevos_cromosomas = [], [], [], []
+        num_rand1, num_rand2 = [ random.random() for _ in range( 6 ) ], [ random.random() for _ in range( 6 ) ]
+        para_crossover = [ 0, 0, 0, 0, 0, 0 ]
+        cp = [ 0, 0, 0, 0, 0, 0 ]
+        pos_mutaciones, genes_mutados = 0, 0
 
         fxs = [ f_deX( cromosoma_1 ), f_deX( cromosoma_2 ), f_deX( cromosoma_3 ), f_deX( cromosoma_4 ), f_deX( cromosoma_5 ), f_deX( cromosoma_6 ) ]
 
-        print( "F(x) = [ ", end="" )
-        print( " ".join( f"{ valor }" for valor in fxs ), end="" )
-        print( " ]" )
+        print( f"Generacion: {generacion}" )
+        print( "F(x) = [ ", end=" " )
+        print( " ".join( f"{ valor }" for valor in fxs ), end=" " )
+        print( " ]\n" )
 
-        fitnesses = [ fitness( fxs[ 0 ] ), fitness( fxs[ 1 ] ), fitness( fxs[ 2 ] ), fitness( fxs[ 3 ] ), fitness( fxs[ 4 ] ), fitness( fxs[ 5 ] ) ]
+        fitnesses = [ fitness( fx ) for fx in fxs ]
 
-        print( "Fitness = [ ", end="" )
-        print( " ".join( f"{valor:.9f} " for valor in fitnesses ), end="" )
-        print( " ]" )
+        print( "Fitness = [ ", end=" " )
+        print( " ".join( f"{valor:.9f} " for valor in fitnesses ), end=" " )
+        print( " ]\n" )
 
         for aptitud in fitnesses:
             totalFitnesses += aptitud
 
-        print( f"Total de fitness de la generación { generacion }" )
-        print( totalFitnesses )
+        print( f"Total de fitness de la generación { generacion }: " )
+        print( totalFitnesses, "\n" )
 
         for fitness_i in fitnesses:
             probabilities.append( fitness_i / totalFitnesses )
 
-        print( "Probabilidades = [ ", end="" )
-        print( " ".join( f"{valor:.9f} " for valor in probabilities ), end="" )
-        print( " ]" )
+        print( "Probabilidades = [ ", end=" " )
+        print( " ".join( f"{valor:.9f} " for valor in probabilities ), end=" " )
+        print( " ]\n" )
 
         for probabilidad in probabilities:
             acum_suma += probabilidad
             acumulaciones.append( acum_suma )
 
-        print( "Probabilidad acumulada = [ ", end="" )
-        print( " ".join( f"{valor:.9f} " for valor in acumulaciones ), end="" )
-        print( " ]" )
+        print( "Probabilidad acumulada = [ ", end=" " )
+        print( " ".join( f"{valor:.9f} " for valor in acumulaciones ), end=" " )
+        print( " ]\n" )
+
+        print( "Numeros random = [ ", end=" " )
+        print( " ".join( f"{valor:.3f} " for valor in num_rand1 ), end=" " )
+        print( " ]\n" )
+
+        i = 0
+
+        while i <= 5:
+            if num_rand1[i] < acumulaciones[0]: 
+                nuevas_pos.append(1)
+            
+            if num_rand1[i] >= acumulaciones[0] and num_rand1[i] <= acumulaciones[1]:
+                nuevas_pos.append(2)
+
+            if num_rand1[i] >= acumulaciones[1] and num_rand1[i] <= acumulaciones[2]:
+                nuevas_pos.append(3)
+            
+            if num_rand1[i] >= acumulaciones[2] and num_rand1[i] <= acumulaciones[3]:
+                nuevas_pos.append(4)
+
+            if num_rand1[i] >= acumulaciones[3] and num_rand1[i] <= acumulaciones[4]:
+                nuevas_pos.append(5)
+
+            if num_rand1[i] >= acumulaciones[4] and num_rand1[i] <= acumulaciones[5]:
+                nuevas_pos.append(6)
+            
+            i += 1
+
+        print( "Nuevas posiciones = [ ", end=" " )
+        print( " ".join( f"{valor}" for valor in nuevas_pos ), end=" " )
+        print( " ]\n" )
+
+        for lugar in nuevas_pos:
+            if lugar == 1:
+                nuevos_cromosomas.append(cromosoma_1[:])
+
+            if lugar == 2:
+                nuevos_cromosomas.append(cromosoma_2[:])
+
+            if lugar == 3:
+                nuevos_cromosomas.append(cromosoma_3[:])
+
+            if lugar == 4:
+                nuevos_cromosomas.append(cromosoma_4[:])
+
+            if lugar == 5:
+                nuevos_cromosomas.append(cromosoma_5[:])
+
+            if lugar == 6:
+                nuevos_cromosomas.append(cromosoma_6[:])
+
+        print("Nuevos cromosomas:", nuevos_cromosomas, "\n")
+
+        nuevo_lugar = 0
+
+        for nc in nuevos_cromosomas:
+            print(nc)
+            if nuevo_lugar == 0:
+                cromosoma_1 = nc
+            if nuevo_lugar == 1:
+                cromosoma_2 = nc
+            if nuevo_lugar == 2:
+                cromosoma_3 = nc
+            if nuevo_lugar == 3:
+                cromosoma_4 = nc
+            if nuevo_lugar == 4:
+                cromosoma_5 = nc
+            if nuevo_lugar == 5:
+                cromosoma_6 = nc
+            
+            nuevo_lugar += 1
+
+        print("Nuevos cromosomas1:", cromosoma_1, "\n")
+        print("Nuevos cromosomas2:", cromosoma_2, "\n")
+        print("Nuevos cromosomas3:", cromosoma_3, "\n")
+        print("Nuevos cromosomas4:", cromosoma_4, "\n")
+        print("Nuevos cromosomas5:", cromosoma_5, "\n")
+        print("Nuevos cromosomas6:", cromosoma_6, "\n")
+
+        print( "Segundo grupo de numeros random = [ ", end=" " )
+        print( " ".join( f"{valor:.3f} " for valor in num_rand2 ), end=" " )
+        print( " ]\n" )
+
+        index = 0
+        for num in num_rand2:
+            if num < 0.25:
+                para_crossover[ index ] = index + 1
+            
+            index += 1
+
+        # Crear una lista temporal para almacenar los valores no cero
+        temp = [valor for valor in para_crossover if valor != 0]
+
+        # Reemplazar los valores en la lista original
+        for i in range(len(para_crossover)):
+            if para_crossover[i] != 0:
+                para_crossover[i] = temp.pop()
+
+        print( "Crossover = [ ", end=" " )
+        print( " ".join( f"{valor} " for valor in para_crossover ), end=" " )
+        print( " ]\n" )
+
+        for i in range(len(cp)):
+            if para_crossover[i] != 0:
+                cp[i] = random.randint(1, len(cromosoma_1) - 1)
+
+        print( "Puntos de corte = [ ", end=" " )
+        print( " ".join( f"{valor} " for valor in cp ), end=" " )
+        print( " ]\n" )
+
+        for index, ncp in enumerate( cp ):
+            if ncp != 0:
+                print(ncp, index)
+
+        for index, valor in enumerate(para_crossover):
+            if valor != 0:
+                cromosoma_1_index = index
+                cromosoma_2_index = valor - 1  # Restamos 1 para ajustar al índice de Python
+                print(cromosoma_1_index, cromosoma_2_index)
+
+                # Cromosomas involucrados en el cruce
+                cromosoma_1 = nuevos_cromosomas[cromosoma_1_index]
+                cromosoma_2 = nuevos_cromosomas[cromosoma_2_index]
+
+                # Puntos de corte
+                puntos_de_corte = cp[index]
+
+                # Realizar el cruce
+                hijo_1 = cromosoma_1[:puntos_de_corte] + cromosoma_2[puntos_de_corte:]
+                hijo_2 = cromosoma_2[:puntos_de_corte] + cromosoma_1[puntos_de_corte:]
+
+                print("Cruce entre cromosoma", cromosoma_1_index + 1, "y cromosoma", cromosoma_2_index + 1)
+                print("Puntos de corte:", puntos_de_corte)
+                print("Hijo 1:", hijo_1)
+                print("Hijo 2:", hijo_2)
+                print()
+        
+        pos_mutaciones = [ random.randint( 1, 24 ) for _ in range( mutaciones_totales ) ]
+
+        if pos_mutaciones[ 0 ] == pos_mutaciones[ 1 ]:
+            while pos_mutaciones[ 0 ] == pos_mutaciones[ 1 ]:
+                pos_mutaciones[ 1 ] = random.randint( 1, 24 )
+
+        genes_mutados = [ random.randint( 1, 30 ) for _ in range( mutaciones_totales ) ]
+
+        print("Posicion de las mutaciones: ", pos_mutaciones )
+        print( "Nuevos genes despues de la mutacion: ", genes_mutados )
 
         generacion += 1 
         

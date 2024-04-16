54023662cc28151aa4f40c110d00abf99527abe1
Jorge Cisneros <jorgecisneros2002@gmail.com>
2024-04-16 11:17:43 -0600

    JAJA

---
+++ genes.py
@@ -0,0 +1,65 @@
+genes_enemigos = {
+    0:,
+    1:,
+    2:,
+    3:,
+    4:,
+    5:,
+    6:,
+    7:,
+    8:,
+    9:,
+    10:,
+    11:,
+    12:,
+    13:,
+    14:
+}
+
+valores_genes_enemigos = {
+
+}
+
+genes_coleccionables = {
+    0:,
+    1:,
+    2:,
+    3:,
+    4:,
+    5:,
+    6:,
+    7:,
+    8:,
+    9:,
+    10:,
+    11:,
+    12:,
+    13:,
+    14:
+}
+
+valores_genes_coleccionables = {
+    
+}
+
+genes_personaje = {
+    0:,
+    1:,
+    2:,
+    3:,
+    4:,
+    5:,
+    6:,
+    7:,
+    8:,
+    9:,
+    10:,
+    11:,
+    12:,
+    13:,
+    14:
+}
+
+valores_genes_personaje = {
+    
+}
